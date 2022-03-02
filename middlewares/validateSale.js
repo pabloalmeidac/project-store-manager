47c5ab1f3b7ca6969006da93cd497901655ea6ce
Pablo Almeida <pabloalmeidac@hotmail.com>
2022-03-01 22:47:09 -0300

    refatorado validacao de sales

--- middlewares/validateSale.js
+++ middlewares/validateSale.js
@@ -1,28 +1,19 @@
 // O Lucas Felipe da turma B me ajudou nesse validate ja que joi me deixou na mão
+const salesSchema = require('../schemas/salesSchema');
 
-const quantityCheck = (quantity) => {
-  if (quantity < 0 || typeof quantity === 'string') {
-    return true;
-  }
-  return false;
-};
 const validateSale = (req, res, next) => {
   const { body } = req;
   body.map(({ productId, quantity }) => {
-    if (!productId) {
-      return res.status(400).json({ message: '"productId" is required' });
-    }
-    if (quantity === 0) {
-      return res.status(422).json({ message: '"quantity" must be greater than or equal to 1' });
-    }
-    if (!quantity) {
-      return res.status(400).json({ message: '"quantity" is required' });
-    }
-    if (quantityCheck(quantity)) {
-      return res.status(422).json({ message: '"quantity" must be greater than or equal to 1' });
+    const { error } = salesSchema.isValid({ productId, quantity });
+        
+    if (error) {
+      // split error do brabo NASC
+      const [code, message] = error.message.split('|');
+      return res.status(code).json({ message });
     }
     return 0;
   });
   return next();
 };
+
 module.exports = validateSale;
